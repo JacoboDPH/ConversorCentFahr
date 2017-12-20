1bae7ef0bd0219770c297e58717a00589c31938f
jacobodph <jacobodhp@me.com>
2017-12-20 01:22:03 +0000

    version 2.0

--- src/Principal.java
+++ src/Principal.java
@@ -3,21 +3,46 @@ import java.util.Scanner; //Importamos la clase Scanner para obtener datos del u
 
 public class Principal {
 
-	private static Scanner teclado = new Scanner (System.in); 
-	
+	private static Scanner teclado = new Scanner(System.in);
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		// Se declaran las varibles que se van a utilizar 
+		 
 		double centigrados, fahrenheit;
+		byte opcion;
+
+		System.out.println(
+				
+				"Elige una opción \n1-Convertir de centígrados a Fahrenheit\n2-Convertir de Fahrenheit a centígrados");
+
+		opcion = teclado.nextByte();
+
+		if (opcion == 1) {
+
+			System.out.println("Introduce los grqdos centígrados:");
+
+			centigrados = teclado.nextDouble();
 		
-		System.out.println("Introduce los grqdos centígrados:"); 
-		
-		centigrados = teclado.nextDouble();
-		
-		fahrenheit = centigrados * (9/5) + 32;
-		
-		System.out.print("Entonces "+centigrados+" grados centigrados son "+ fahrenheit + " grados fahrenheit");
-		
+			fahrenheit = 32+((9*centigrados)/5);
+
+			System.out
+					.print("Entonces " + centigrados + " grados centigrados son " + fahrenheit + " grados fahrenheit");
+
+		} else if (opcion == 2) {
+
+			System.out.println("Introduce los grqdos Fehrenheit:");
+
+			fahrenheit = teclado.nextDouble();
+
+			centigrados = 5*((fahrenheit -32 )/9);
+			
+					
+			System.out
+					.print("Entonces " + centigrados + " grados centigrados son " + fahrenheit + " grados fahrenheit");
+			
+		}
 		
 	}
 
